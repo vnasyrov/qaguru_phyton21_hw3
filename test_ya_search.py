a31d17c15cab36324e36c438d71a8258a10360c7
Valeriy Nasyrov <valerius.nasyrov@yandex.ru>
2025-08-11 20:33:55 +0300

    Исправил зависимости в requirements.txt

--- test_ya_search.py
+++ test_ya_search.py
@@ -2,19 +2,21 @@ import pytest
 from selene import browser, have
 
 
-@pytest.fixture(scope="session")
-def open_browser():
+@pytest.fixture(scope="function")
+def setup_browser():
     browser.config.window_width = 1280
     browser.config.window_height = 800
+    browser.config.timeout = 6  # увеличил таймаут ожидания до 6 сек
     browser.open('https://ya.ru')
     yield
     browser.quit()
 
-
-def test_input_value(open_browser):
-    browser.element('[id="text"]').type('погода Санкт-Петербург').press_enter()
+def test_input_value(setup_browser):
+    browser.element('#text').type('погода Санкт-Петербург').press_enter()
     browser.element('input[name="text"]').should(have.value('погода Санкт-Петербург'))
 
-def test_broke_search(open_browser):
-    browser.element('[id="text"]').type('"Жаброкогтельный флюкскварц 38QZ-LR-α"').press_enter()
-    browser.element('[class="RequestMeta-Message"]').should(have.text('Точного совпадения не нашлось. Показаны результаты по запросу без кавычек.'))
+def test_broke_search(setup_browser):
+    query = '"Жаброкогтельный флюкскварц 38QZ-LR-α"'
+    browser.element('#text').type(query).press_enter()
+    browser.element('.RequestMeta-Message').should(have.text('Точного совпадения не нашлось. Показаны результаты по запросу без кавычек.')
+    )
